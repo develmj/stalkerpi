3eaa2fe592932aa6a613072233a8538221c4fe48
mj <develmj@gmail.com>
2016-02-14 15:31:27 +0530

    Code errors

--- distance.py
+++ distance.py
@@ -28,18 +28,18 @@ try:
         while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
             pulse_start = time.time()              #Saves the last known time of LOW pulse
 
-            while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
-                pulse_end = time.time()                #Saves the last known time of HIGH pulse
+        while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+            pulse_end = time.time()                #Saves the last known time of HIGH pulse
 
-                pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+        pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
 
-                distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
-                distance = round(distance, 2)            #Round to two decimal points
+        distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+        distance = round(distance, 2)            #Round to two decimal points
 
-                if distance > 2 and distance < 400:      #Check whether the distance is within range
-                    print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
-                else:
-                    print "Out Of Range"                   #display out of range
+        if distance > 2 and distance < 400:      #Check whether the distance is within range
+            print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+        else:
+            print "Out Of Range"                   #display out of range
 except KeyboardInterrupt:
     pass
 finally:
